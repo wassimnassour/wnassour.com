70a68197a7498a99a0c49c402c7e0a1a8c55f48e
wassimnassour <nassourwassim@gmail.com>
2021-07-25 12:41:20 +0100

    build🔨🏗: add navbar and footer to the layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
+/** @jsxImportSource theme-ui */
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'theme-ui'
-import { theme } from '../theme'
+import { theme } from '../ui/theme'
+import { Footer, Nav } from 'components'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <div sx={{ bg: '#0e1630fa', minHeight: '90vh' }}>
+        <Nav />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </ThemeProvider>
   )
 }
