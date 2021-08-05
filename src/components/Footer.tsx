5df30fb455ef5aeb67d086f87b869a162925c838
wassim <nassourwassim@gmail.com>
2021-08-05 13:07:26 +0100

    refactor: footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,26 +13,9 @@ export const Footer = () => {
         alignItems: 'center',
       }}
     >
-      <h4 sx={{ color: 'gainsboro' }}>
-        {' '}
+      <h4 sx={{ color: 'gainsboro', textAlign: 'center' }}>
         Created by @Wassim built with @NextJs deployed in @Vercel
       </h4>
-      {/* <div
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          width: '70%',
-          borderBottom: '1px solid #EB4A4A',
-        }}
-      >
-        <ul sx={{ display: 'flex', ...styles.ul }}>
-          <li sx={styles.li}>Home</li>
-          <li sx={styles.li}>blog</li>
-          <li sx={styles.li}>About me</li>
-          <li sx={styles.li}>contact me </li>
-        </ul>
-        <h4 sx={{ color: 'gainsboro' }}>Created by @Wassim</h4>
-      </div> */}
     </div>
   )
 }
