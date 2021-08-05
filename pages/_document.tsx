5472bf4bcd2c7edf82054c854c1f00c35a68614c
wassim <nassourwassim@gmail.com>
2021-08-05 13:31:32 +0100

    fix: issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap"
             rel="stylesheet"
