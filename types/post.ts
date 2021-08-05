122f5d58c7c29e6899e9e071f8b4f5cd9d39c7f2
wassim <nassourwassim@gmail.com>
2021-08-05 13:21:13 +0100

    build: folder contain typeScript types

---
+++ types/post.ts
@@ -0,0 +1,13 @@
+export interface IPost {
+  slug: string
+  date: string
+  thumbnail: string
+  title: string
+  description: string
+  excerpt: string
+  yields: string
+  ingredients: string[]
+  directions: string[]
+  tips: string[]
+  author: string
+}
