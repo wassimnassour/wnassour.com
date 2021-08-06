2bc46cd10f63bf248ee8ecc9ff52a63d01246aeb
wassim <nassourwassim@gmail.com>
2021-08-06 10:01:23 +0100

    build: update SITE_URL

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
-export const SITE_URL = 'http://localhost:3000'
+export const SITE_URL =
+  'https://portfolio-v2-1pnk9vjvk-wassimnassour.vercel.app/'
 export const SITE_NAME = 'wassim Blog'
 export const TWITTER_USERNAME = '@wassimnassour'
