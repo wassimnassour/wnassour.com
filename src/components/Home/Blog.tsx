c8a1a2634a3a870f3b1d28b382c4dc54d8d47a29
wassimnassour <nassourwassim@gmail.com>
2021-09-23 12:49:22 +0100

    build: update posts ui in home page

--- src/components/Home/Blog.tsx
+++ src/components/Home/Blog.tsx
@@ -14,61 +14,82 @@ export const Blog = ({ Posts }: Props) => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        my: 3,
-        mt: 4,
+        my: 4,
+        mt: [3, 3, 6],
       }}
     >
       <div sx={{ width: ['90%', '90%', 800, '75%'], maxWidth: 1100 }}>
-        <div sx={{ display: 'flex', alignItems: 'center' }}>
-          <span
+        <div
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
             sx={{
-              display: 'inline-block',
-              bg: 'secondary',
-              height: 2,
-              width: 30,
-              mr: 3,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
-          ></span>
-          <h2 sx={{ color: 'white', fontSize: 32 }}>Blog</h2>
+          >
+            <span
+              sx={{
+                display: 'inline-block',
+                bg: 'secondary',
+                height: 2,
+                width: 30,
+                mr: 3,
+              }}
+            ></span>
+            <h2 sx={{ color: 'white', fontSize: 32 }}>Blog</h2>
+          </div>
+
+          <Link href="/blog">
+            <a
+              sx={{
+                color: 'whitesmoke',
+                my: 3,
+                display: 'block',
+                textDecoration: 'underline',
+              }}
+            >
+              Read more &gt;
+            </a>
+          </Link>
         </div>
         <div
           sx={{ display: 'flex', flexDirection: ['column', 'column', 'row'] }}
         >
           {Posts.map((post) => (
-            <div
-              key={post.title}
-              sx={{
-                flex: [1, 1 / 2, 1 / 3],
-                mr: [0, 0, 3, 4],
-                my: [1, 1, 0],
-                p: 3,
-                borderRadius: 7,
-                bg: '#49536b',
-              }}
-            >
-              <span sx={{ color: 'gainsboro', m: 1 }}>{post.date}</span>
-              <h1 sx={{ fontSize: 22, color: 'white' }}>{post.title}</h1>
-              <p sx={{ fontSize: 16, color: 'whitesmoke' }}>{post.excerpt}</p>
-              <Link href={`/posts/${post.slug}`}>
-                <a sx={{ color: 'white', fontWeight: 700, cursor: 'pointer' }}>
-                  Read More
-                </a>
-              </Link>
-            </div>
+            // eslint-disable-next-line @next/next/link-passhref
+            <Link href={`/posts/${post.slug}`} key={post.title}>
+              <a sx={{ cursor: 'pointer' }}>
+                <div
+                  sx={{
+                    flex: [1, 1 / 2, 1 / 3],
+                    mr: [0, 0, 3, 3],
+                    my: [2, 2, 0],
+                    p: 2,
+                    borderRadius: 2,
+                    bg: '#49536b',
+                  }}
+                >
+                  <h1 sx={{ fontSize: 22, color: 'white' }}>{post.title}</h1>
+                  <p sx={{ fontSize: 16, color: 'whitesmoke' }}>
+                    {post.excerpt.substring(1, 400)}...
+                  </p>
+                  <div
+                    sx={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    <span sx={{ color: 'gainsboro', m: 1 }}>{post.date}</span>
+                    <span sx={{ color: 'gainsboro', m: 1 }}>4 min read</span>
+                  </div>
+                </div>
+              </a>
+            </Link>
           ))}
         </div>
-        <Link href="/blog">
-          <a
-            sx={{
-              color: 'whitesmoke',
-              my: 3,
-              display: 'block',
-              textDecoration: 'underline',
-            }}
-          >
-            Read more &gt;
-          </a>
-        </Link>
       </div>
     </div>
   )
