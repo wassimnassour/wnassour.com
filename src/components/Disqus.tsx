66051968034aacfb6f8d44d4991be8c4b60525ee
wassimnassour <nassourwassim@gmail.com>
2021-09-25 11:17:55 +0100

    🏗 Disque components

---
+++ src/components/Disqus.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource theme-ui */
+import { jsx } from 'theme-ui'
+import { DiscussionEmbed } from 'disqus-react'
+import { SITE_URL } from 'utils'
+import { IPost } from '../../types/post'
+
+interface Props {
+  slug: string
+  post: Omit<IPost, 'slug'>
+}
+
+const DisqusComments = ({ post, slug }: Props) => {
+  const disqusShortname = 'wassim'
+  const disqusConfig = {
+    url: SITE_URL + slug,
+    identifier: post.title,
+    title: post.title,
+  }
+  return (
+    <div sx={{ my: 5 }}>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </div>
+  )
+}
+export default DisqusComments
