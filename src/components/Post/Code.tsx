14cfe438bfa5524eb43697381620cb52cb51dec9
wassimnassour <nassourwassim@gmail.com>
2021-09-28 23:10:07 +0100

    do some chanegs on code components

--- src/components/Post/Code.tsx
+++ src/components/Post/Code.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource theme-ui */
 import { jsx } from 'theme-ui'
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/dracula'
+import theme from 'prism-react-renderer/themes/nightOwl'
 import React, { useState } from 'react'
 
 export const CodeBlock = ({
@@ -10,52 +10,53 @@ export const CodeBlock = ({
 }: {
   children: string
   language: Language
-}) => (
-  <Highlight
-    {...defaultProps}
-    theme={theme}
-    code={children}
-    language={language}
-  >
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <pre
-        sx={{
-          borderRadius: 10,
-          bg: '#262b3e !important',
-          textAlign: 'left',
-          my: '2em',
-          py: 3,
-          px: 0.6,
-          overflow: 'scroll',
-        }}
-        className={className}
-        style={style}
-      >
-        {tokens.map((line, i) => (
-          <div
-            sx={{ display: 'table-row' }}
-            key={i}
-            {...getLineProps({ line, key: i })}
-          >
-            <span
-              sx={{
-                display: 'table-cell',
-                textAlign: 'right',
-                paddingRight: '1em',
-                userSelect: 'none',
-                opacity: 0.5,
-              }}
+}) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          sx={{
+            borderRadius: 2,
+            bg: '#01203a !important',
+            textAlign: 'left',
+            my: '2em',
+            py: 3,
+            overflow: 'scroll',
+          }}
+          className={className}
+          style={style}
+        >
+          {tokens.map((line, i) => (
+            <div
+              sx={{ display: 'table-row' }}
+              key={i}
+              {...getLineProps({ line, key: i })}
             >
-              {i + 1}
-            </span>
-            <span sx={{ display: 'table-cell', fontSize: 15 }}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </span>
-          </div>
-        ))}
-      </pre>
-    )}
-  </Highlight>
-)
+              <span
+                sx={{
+                  display: 'table-cell',
+                  textAlign: 'right',
+                  paddingRight: '1em',
+                  userSelect: 'none',
+                  opacity: 0.5,
+                }}
+              >
+                {i + 1}
+              </span>
+              <span sx={{ display: 'table-cell', fontSize: 15 }}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </span>
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
