3eb032e393a42ff12ac46029d246d886c98e7fc8
wassimnassour <nassourwassim@gmail.com>
2021-09-28 16:08:45 +0100

    add images config to next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   env: {
     DISABLE_NEW_JSX_TRANSFORM: process.env.DISABLE_NEW_JSX_TRANSFORM,
   },
+  images: {
+    domains: ['lh6.googleusercontent.com', 'media.giphy.com'],
+  },
 }
