e7cff96d22a72a93cc29adb830b909cba8dfe3a4
nassourwassim <nassourwassim@gmail.com>
2021-11-24 22:01:10 +0100

    🪛 fix issue with google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,22 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            id="gtag-init"
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
