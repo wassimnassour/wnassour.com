56d5e7b89aacdee88ff881b032898e6fb79c4451
nassourwassim <nassourwassim@gmail.com>
2023-05-25 18:54:43 +0100

    updates

--- src/components/Post/Tag.tsx
+++ src/components/Post/Tag.tsx
@@ -2,7 +2,7 @@ import { MdxComponent } from 'types'
 
 export const Tag = ({ children }: MdxComponent) => {
   return (
-    <div className="inline-block px-[6px] py-[3px] mx-1 text-[#ef7575] text-sm bg-gray-800 rounded-md w-fit">
+    <div className="inline-block px-[6px] py-[3px] mx-1 text-[#ffffff] text-sm bg-gray-700 rounded-md w-fit">
       {children}
     </div>
   )
