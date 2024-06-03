055f3c6e9ac93729725b1f6933ff7b963b963329
Wassin NASSOUR <nassourwassin@MAR-MBP044.local>
2024-06-03 16:42:59 +0100

    fix font list

--- src/components/Post/Li.tsx
+++ src/components/Post/Li.tsx
@@ -3,7 +3,12 @@ import cx from 'utils/cx'
 
 export const Li = ({ children, className }: MdxComponent) => {
   return (
-    <li className={cx('mt-3  text-secondary list-red list-inside', !!className && className)}>
+    <li
+      className={cx(
+        'mt-3  text-secondary  text-base list-red list-inside',
+        !!className && className
+      )}
+    >
       <div className="inline text-white ">{children}</div>
     </li>
   )
